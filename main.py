a352e00045f0b073d09c68968a847674aa09c31c
Zach Harding <zharding@bishopfox.com>
2025-10-04 15:15:19 -0400

    Add gemini client

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
+
+
 def main():
     initialize_from_persist_environment()
     #This is the main game loop, as of not the global DB will be used.
@@ -9,6 +15,29 @@ def main():
      persist_environment()
 
     
+def initialize_from_persist_environment():
+    #Get secrets from .env 
+    load_dotenv()
+    #Not needed?g
+    gemini_api_key = os.getenv(GEMINI_API_KEY)
+    client = genai.Client()
+
+    response = client.models.generate_content(
+        model="gemini-2,5-pro", contents="Respond with 123"
+    )
+    print(response.text)
+
+def process_user_input():
+    pass
+
+def update_environment():
+    pass
+
+def generative_content():
+    pass
+
+def persist_environment():
+    pass
 
 if __name__ == "__main__":
     main()
