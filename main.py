612f8c6d7d42242d0cc1ee3c995537ab24b13ee7
Zach Harding <zharding@bishopfox.com>
2025-10-04 15:00:07 -0400

    Outline for game loop.

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 def main():
-    print("Hello World 2")
+    initialize_from_persist_environment()
+    #This is the main game loop, as of not the global DB will be used.
+    while True:
+     #This will halt until user input is received
+     process_user_input()
+     update_environment()
+     generative_content()
+     persist_environment()
+
+    
 
 if __name__ == "__main__":
     main()
